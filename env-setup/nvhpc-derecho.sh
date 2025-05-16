be10ef8b4f5c6790cf6954e6b417ef0900c0dc4f
Jim W <70100781+jim-p-w@users.noreply.github.com>
2025-05-16 12:29:05 -0600

    Add nvhpc (gpu) toolchain support to cmake (#170)
    cmake only configures MPAS-Model if the gpu toolchain is loaded.
    Add bash and csh scripts to load the gpu toolchain on derecho.
    
    Modify env-setup/run_make.bundle.sh to create pbs job which runs nvphc tools.

---
+++ env-setup/nvhpc-derecho.sh
@@ -0,0 +1,25 @@
+
+if [ "$NCAR_HOST" != "derecho" ]; then
+  echo $0 must be run on derecho
+  return
+fi
+source /etc/profile.d/z00_modules.sh
+
+module --force purge
+module load ncarenv/23.09
+
+# ecbuild requires either stack-gcc/12.2.0 or stack-intel/2021.10.0
+module use /glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core
+module load stack-gcc/12.2.0
+module load ecbuild
+module load cmake/3.26.3 2> /dev/null
+
+module load craype/2.7.23
+module load nvhpc/24.9 2> /dev/null
+module load ncarcompilers/1.0.0
+module load gcc-toolchain/13.2.0 2> /dev/null
+module load cray-mpich/8.1.29 2> /dev/null
+module load cuda/12.2.1
+module load parallel-netcdf/1.12.3
+module list
+
