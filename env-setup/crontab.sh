698c8eaa57cb7e7acfd321eae7876503ac348284
Jim W <70100781+jim-p-w@users.noreply.github.com>
2026-02-23 10:02:21 -0700

    Allow specifying separate directories for mpas-bundle source and build trees. (#181)
    This allows putting the build output on scratch
    and the source somewhere else (not scratch).

--- env-setup/crontab.sh
+++ env-setup/crontab.sh
@@ -5,9 +5,12 @@
 # 2. run: crontab crontab.sh
 
 # set this to the directory the mpas-bundle repository has been cloned to
-bundle_dir=/glade/derecho/scratch/jwittig/repos-s/mpas-bundle-cron/mpas-bundle
+# don't put it on scratch or it will get corrupted due to scratch retention policy
+bundle_dir=/glade/work/jwittig/repos1/mpas-bundle-cron-src/mpas-bundle
+# set this to where the builds are - we want them on scratch so they get cleaned up
+builds_dir=/glade/derecho/scratch/jwittig/repos-s/mpas-bundle-cron/
 # the directory where the single precision bundle build is
-bundle_build_dir=/glade/derecho/scratch/jwittig/repos-s/mpas-bundle-cron//build-gnu-1p_
+bundle_build_dir="'/glade/derecho/scratch/jwittig/repos-s/mpas-bundle-cron/build-gnu-1p_[01]'"
 # this is the build script to execute, relative to the mpas-bundle source directory
 build_script=env-setup/mpas-bundle-cron.sh
 
@@ -19,6 +22,7 @@ workflow_dir=/glade/derecho/scratch/jwittig/repos-s/MPAS-Workflow-cron
 workflow_script=env-setup/run_cylc.sh
 # the workflow scenario to run, relative to the workflow directory
 workflow_scenario=scenarios/3denvar_OIE120km_WarmStart_VarBC_cron.yaml
+workflow_scenario2=scenarios/3dhybrid_OIE120km_WarmStart_cron.yaml
 
 # the directory with the graphics scripts
 graphs_dir=/glade/derecho/scratch/jwittig/repos-s/mpas-jedi-cron/graphics
@@ -28,7 +32,7 @@ graphs_out_dir=/glade/derecho/scratch/jwittig/graphs/data/
 # derecho hpc
 derecho=derecho.hpc.ucar.edu
 # mmm web server
-webserver=whitedwarf.mmm.ucar.edu
+webserver=eris.mmm.ucar.edu
 # destination for graphs
 web_graphs_dir=/web/htdocs/projects/mpas-jedi/weekly-cycling/cylc_graphs
 
@@ -39,28 +43,37 @@ web_graphs_dir=/web/htdocs/projects/mpas-jedi/weekly-cycling/cylc_graphs
 lastmon=$(date --date='-1 month' +%Y-%m)
 30 7 7 * * ssh $derecho "cd ~/my_cron_logs/cylc/logs && tar --remove-files -czf run_cylc.cron.$lastmon.tgz *.$lastmon*.log"
 
+bld_suffix=date +%0m_%0d_%y
+
 # start at 12:05 AM on Mon-Fri
 # make a double precision build of mpas-bundle and run ctest
 # the build script won't do anything if there have been no changes to the
 # modules used to create mpas-bundle (unless the '-f' parameter is provided).
-05 00 * * 1-5 ssh $derecho "cd $bundle_dir && git fetch -p >> ~/my_cron_logs/cron.log ; git status >> ~/my_cron_logs/cron.log ; git update >> ~/my_cron_logs/cron.log" ;$bundle_dir/$build_script -d $bundle_dir -q develop@desched1 -c gnu -p 2 -a nmmm0043
+05 00 * * 1-5 ssh $derecho "echo ${bld_suffix} ${bundle_dir} 'make 2p'  >> ~/my_cron_logs/cron.log && cd $bundle_dir && git fetch -p >> ~/my_cron_logs/cron.log ; git status >> ~/my_cron_logs/cron.log ; git pull >> ~/my_cron_logs/cron.log" ;$bundle_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c gnu -p 2 -a nmmm0015
 
 # start at 12:05 AM on Sat 
 # always build, even if no source change from previous run (-f)
-05 00 * * 6 $bundle_dir/$build_script -d $bundle_dir -q develop@desched1 -c gnu -p 2 -f
+05 00 * * 6 $bundle_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c gnu -p 2 -f -a nmmm0015
 
 # start at 1:05 AM on Sat 
 # always build, even if no source change from previous run (-f)
 # single precision (-p 1), to be used for cylc experiment.
 # use the date as part of the build directory name, so each week's build is unique.
-bld_suffix=date +%0m_%0d_%y
-05 01 * * 6 $bundle_dir/$build_script -d $bundle_dir -q develop@desched1 -c gnu -p 1 -f -l $bundle_dir/$build_script.lock -x $(${bld_suffix}) -a nmmm0043
+05 01 * * 6 $bundle_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c gnu -p 1 -f -l $bundle_dir/$build_script.lock -x $(${bld_suffix}) -a nmmm0015
+# build gpu enabled MPAS-Model
+#20 12 * * 1 $bundle_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c nvhpc -p 1 -f -l $bundle_dir/$build_script.lock -x $(${bld_suffix}) -a nmmm0015
 
 # at 12:05 am on Sun update the develop branch of  MPAS-Workflow repo and run the workflow
 suffix=$(date +%F)
-05 00 * * 7 ssh $derecho "cd $workflow_dev_dir && git fetch -p >> ~/my_cron_logs/cron.log && git co develop >> ~/my_cron_logs/cron.log && git pull >> ~/my_cron_logs/cron.log && $workflow_dir/$workflow_script -w $workflow_dev_dir -d $bundle_build_dir -k $bundle_dir/$build_script.lock -s $workflow_scenario -x $suffix"
+05 00 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> ~/my_cron_logs/cron.log && cd $workflow_dir && git fetch -p >> ~/my_cron_logs/cron.log 2>&1 && git co develop >> ~/my_cron_logs/cron.log 2>&1 && git pull >> ~/my_cron_logs/cron.log 2>&1 && $workflow_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $bundle_dir/$build_script.lock -s $workflow_scenario -x $suffix"
+# at 1:05 am on Sun run the 3dhybrid workflow
+05 01 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> ~/my_cron_logs/cron.log && cd $workflow_dir && $workflow_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $bundle_dir/$build_script.lock -s $workflow_scenario2 -x $suffix"
+
+# run the weekly cylc scenario using a gpu build of MPAS-Model
+suffix_gpu=2025-05-30_new
+workflow_scenario_gpu=scenarios/3denvar_OIE120km_WarmStart_VarBC_gpu_cron.yaml
+#55 15 * * * ssh $derecho "echo $suffix >> ~/my_cron_logs/cron.log 2>&1 && cd $workflow_dev_dir && git fetch -p >> ~/my_cron_logs/cron.log 2>&1 && git co develop >> ~/my_cron_logs/cron.log 2>&1 && git pull >> ~/my_cron_logs/cron.log && $workflow_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $bundle_dir/$build_script.lock -s $workflow_scenario_gpu -x ${suffix}_gpu"
 
 # at 2:05 am Mon-Fri try to graph results from the completed workflow runs
 casper=casper.hpc.ucar.edu
 05 02 * * 1-5 ssh $casper "$workflow_dir/$workflow_script -w $workflow_dir -g $graphs_dir -o $graphs_out_dir -m $webserver -c $web_graphs_dir/"
-
--- env-setup/mpas-bundle-cron.sh
+++ env-setup/mpas-bundle-cron.sh
@@ -157,7 +157,7 @@ run_cmake()
 cat > $cmake_script << CMAKE_EOF
 #!/bin/bash
 #
-cd $1 && source ../mpas-bundle/env-setup/$2-derecho.sh && if [ -f Makefile ]; then echo "make update" && make update |& tee make.update.log; fi && cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DBUNDLE_SKIP_RTTOV=ON -DMPAS_DOUBLE_PRECISION=$3 ctest_update  ../mpas-bundle;
+cd $1 && source $2/env-setup/$3-derecho.sh && if [ -f Makefile ]; then echo "make update" && make update |& tee make.update.log; fi && cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DBUNDLE_SKIP_RTTOV=ON -DMPAS_DOUBLE_PRECISION=$4 ctest_update  $2;
 CMAKE_EOF
 
   chmod 755 ${cmake_script}
@@ -316,7 +316,7 @@ make_html()
   log "            chref=$href sha_file=$sha_file sha_file:t=${sha_file##*/}"
 
   # get tool versions to put into summary table
-  local spack_stack=$(grep spack-stack- ../mpas-bundle/env-setup/${cc}-derecho.sh | awk -F/ '{print $8}')
+  local spack_stack=$(grep spack-stack- ${BUNDLE_DIR}/env-setup/${cc}-derecho.sh | awk -F/ '{print $8}')
   log "spack-stack: ${spack_stack}"
   if [ ${cc} == "gnu" ]; then
     local comp="gcc"
@@ -325,9 +325,9 @@ make_html()
   elif [ ${cc} == "nvhpc" ]; then
     local comp="nvhpc"
   fi
-  local compiler=$(grep "load .*${comp}" ../mpas-bundle/env-setup/${cc}-derecho.sh | awk '{print $3}')
+  local compiler=$(grep "load .*${comp}" ${BUNDLE_DIR}/env-setup/${cc}-derecho.sh | awk '{print $3}')
   log "compiler: ${compiler}"
-  local mpich=$(grep "load .*cray-mpich" ../mpas-bundle/env-setup/${cc}-derecho.sh | awk '{print $3}')
+  local mpich=$(grep "load .*cray-mpich" ${BUNDLE_DIR}/env-setup/${cc}-derecho.sh | awk '{print $3}')
   log "mpich: ${mpich}"
 
 
@@ -399,7 +399,7 @@ build_and_test()
 
   #--------------------------------------------------------------
   # go to build directory, exit on failure:
-  BUILD_DIR="${BUNDLE_DIR}/../build-${cc}-${build_dir_suffix}"
+  BUILD_DIR="${BUILD_DIR_ROOT}/build-${cc}-${build_dir_suffix}"
   if [ "$suffix" != "" ]; then
     BUILD_DIR="${BUILD_DIR}_${suffix}"
   fi
@@ -413,7 +413,7 @@ build_and_test()
   # run cmake on a login node (compute nodes have poor internet transmission)
   # block until cmake finishes
   if [[ "$run_cmake" == "yes" ]]; then
-    run_cmake ${BUILD_DIR} ${cc} $dbl_p
+    run_cmake ${BUILD_DIR} ${BUNDLE_DIR} ${cc} $dbl_p
   fi
 
   # create script to run gnu make and run it
@@ -466,6 +466,7 @@ build_and_test()
 CTEST_LOGFILE="ctest.pbs.sh.log"
 QUEUE=$MAIN_Q
 BUNDLE_DIR=""
+BUILD_DIR_ROOT=""
 tools=""
 precision="2"
 force_build=0
@@ -476,10 +477,11 @@ suffix=""
 ACCOUNT="nmmm0015"
 
 # get comamnd line args
-while getopts d:q:a:p:c:l:o:x:fhn flag
+while getopts d:b:q:a:p:c:l:o:x:fhn flag
 do
   case "${flag}" in
     d) BUNDLE_DIR="${OPTARG}";;
+    b) BUILD_DIR_ROOT="${OPTARG}";;
     q) QUEUE="${OPTARG}";;
     a) ACCOUNT="${OPTARG}";;
     p) precision=${OPTARG};;
@@ -505,9 +507,19 @@ if [ "$BUNDLE_DIR" = "" ]; then
   log "source dir <-d source_dir> is required"
   usage
 fi
+
 if [ ! -d ${BUNDLE_DIR} ]; then
   log "source dir ${BUNDLE_DIR} does not exist"
   usage
+fi
+
+if [ "$BUILD_DIR_ROOT" == "" ]; then
+  BUILD_DIR_ROOT="${BUNDLE_DIR}/.."
+else
+  if [ ! -d ${BUILD_DIR_ROOT} ]; then
+    log "output dir ${BUILD_DIR_ROOT} does not exist"
+    usage
+  fi
 fi
 
 if [[ "$QUEUE" != "$MAIN_Q" && "$QUEUE" != "$PREEMPT_Q" && "$QUEUE" != "$DEV_Q" ]]; then
